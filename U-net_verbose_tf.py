5053ffb462f590a7e232ae5cbee7ca74b843f1b8
Ahmed ElGazzar <gazzar033@gmail.com>
2017-02-10 08:47:49 +0100

    Update U-net_verbose_tf.py

--- U-net_verbose_tf.py
+++ U-net_verbose_tf.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import cv2
 import os
 import numpy as np
-from keras.metrics import fscore, precision,fmeasure,recall,fbeta_score
+from keras.metrics import fscore,fmeasure,recall,fbeta_score,binary_accuracy,precision
 from keras.models import Model
 from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D, Dropout
 from keras.optimizers import Adam, SGD
@@ -31,13 +31,110 @@ def load_traindata():
 
 def load_testdata():
     path = os.getcwd()
-    imgs_test=np.load(path + "/data/imgs_train_128_3FLAIR.npy")
-    imgs_mask_test=np.load(path + "/data/gt_train_128_3.npy")
-    #imgs_test = imgs_test.astype('float32')
-    #imgs_mask_test = imgs_mask_test.astype('float32')
-    return  imgs_test , imgs_mask_test
+    x = np.load(path + "/data/imgs_train_256_40_T1.npy")
+    y = np.load(path + "/data/gt_train_256_40.npy")
+    print (x.shape)
+    print (y.shape)
+    #imgs_train = imgs_train.astype('float32')
+    #imgs_mask_train = imgs_mask_train.astype('float32')
+    return  x, y
 
-#Loss Function
+#Evaluations
+def evaluate(y_true, y_pred):
+    labels = K.flatten(y_true)
+    labels = tf.to_int64(labels)
+    labels = tf.one_hot(labels,2)
+    logits = tf.reshape(y_pred, (-1, 2))
+
+    acc = binary_accuracy(labels,logits)
+    pr = precision(labels,logits)
+    rec = recall(labels,logits)
+    fm = fmeasure(labels,logits)
+
+
+
+def acc1(y_true, y_pred):
+    labels = K.flatten(y_true)
+    labels = tf.to_int64(labels)
+    labels = tf.one_hot(labels,2)
+    logits = tf.reshape(y_pred, (-1, 2))
+
+    return binary_accuracy(labels,logits)
+
+
+def prec1(y_true, y_pred):
+    labels = K.flatten(y_true)
+    labels = tf.to_int64(labels)
+    labels = tf.one_hot(labels,2)
+    logits = tf.reshape(y_pred, (-1, 2))
+
+    return precision(labels,logits)
+
+
+
+def rec1(y_true, y_pred):
+    labels = K.flatten(y_true)
+    labels = tf.to_int64(labels)
+    labels = tf.one_hot(labels,2)
+    logits = tf.reshape(y_pred, (-1, 2))
+
+
+    return  recall(labels,logits)
+
+
+def fm1(y_true, y_pred):
+    labels = K.flatten(y_true)
+    labels = tf.to_int64(labels)
+    labels = tf.one_hot(labels,2)
+    logits = tf.reshape(y_pred, (-1, 2))
+
+    return fmeasure(labels,logits)
+
+
+def prec(y_true, y_pred):
+    labels = K.flatten(y_true)
+    #labels = tf.to_int64(labels)
+    logits = tf.reshape(y_pred, (-1, 2))
+    logits = tf.argmax(logits,1)
+    logits = tf.to_float(logits)
+
+    true_positives = np.sum(K.round(labels * logits))
+    print ("Shape:" , true_positives)
+    predicted_positives = np.sum(K.round(K.clip(logits, 0, 1)))
+    print ("Shape:" , predicted_positives)
+    precision = true_positives / (predicted_positives + K.epsilon())
+    print ("Shape:" , precision)
+    return precision
+
+
+def rec(y_true, y_pred):
+    labels = K.flatten(y_true)
+    #labels = tf.to_int64(labels)
+    logits = tf.reshape(y_pred, (-1, 2))
+    logits = tf.argmax(logits,1)
+    logits = tf.to_float(logits)
+
+    true_positives = np.sum(K.round(K.clip(labels * logits, 0, 1)))
+    possible_positives = np.sum(K.round(K.clip(labels, 0, 1)))
+
+    rec = true_positives / (possible_positives+K.epsilon())
+    return rec
+
+
+
+def fm(y_true, y_pred):
+    p = prec(y_true, y_pred)
+    r = rec(y_true, y_pred)
+    print(p)
+
+    bb = 1 ** 2
+    fm = (1 + bb) * (p * r) / (bb * p + K.epsilon() )
+    fm = tf.to_int64(fm)
+    return fm
+
+
+
+
 def dice_coef(y_true, y_pred):
 
     labels = y_true
@@ -49,7 +146,6 @@ def dice_coef(y_true, y_pred):
     union = tf.reduce_sum(tf.mul(flat_logits, flat_logits), 1, keep_dims=True) \
                     + tf.reduce_sum(tf.mul(flat_labels, flat_labels), keep_dims=True)
     return 2 * intersection/ (union)
-
 
 
 def dice_coef_loss(y_true, y_pred):
@@ -65,45 +161,9 @@ def sigmoid_cross_weighted(y_true,y_pred):
     return tf.reduce_mean(cross_entropy, name = 'class_cross_entropy')
 
 
-def softmax_cross(y_true,y_pred):
 
-    print ("labels Shape: ", y_true.get_shape()) # shape [batch_size*length*width, 1]
-    ratio = 1 / (100)
-    class_weight = tf.constant([ratio*1.0,1.0-ratio])
-    logits = tf.reshape(y_pred, (-1, 2))# shape [batch_size*length*width, 2]
-    print ("logits: ", tf.Print(logits,[logits])) # shape [batch_size,length,width, 2]
-    weighted_logits = tf.mul(logits, class_weight) # shape [batch_size*length*width, 2]
-    print ("Weighted logits: ", tf.Print(weighted_logits,[weighted_logits])) # shape [batch_size,length,width, 2]
-    labels = tf.to_int64(y_true)
-    labels = K.flatten(labels)# shape [batch_size*length*width]   Flattening
-    print ("labels: ", tf.Print(labels,[labels]))
-    labels = tf.one_hot(labels,2)# shape [batch_size*length*width, 2]   One hot encoding
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
-      weighted_logits, labels, name="None")
-
-    return tf.reduce_mean(cross_entropy, name = 'class_cross_entropy')
-
-def softmax_cross2(y_true,y_pred):
-
-    print ("labels Shape: ", y_true.get_shape()) # shape [batch_size*length*width, 1]
-    #ratio = 1 / (100)
-    class_weight = tf.constant([[1.0, 1000]])
-    logits = tf.reshape(y_pred, (-1, 2))# shape [batch_size*length*width, 2]
-    print ("logits: ", tf.Print(logits,[logits])) # shape [batch_size,length,width, 2]
-
-
-
-    labels = tf.to_int64(y_true)
-    labels = K.flatten(labels)# shape [batch_size*length*width]   Flattening
-    print ("labels: ", tf.Print(labels,[labels]))
-    labels = tf.one_hot(labels,2)# shape [batch_size*length*width, 2]   One hot encoding
-    weight_per_label = tf.transpose( tf.matmul(labels  , tf.transpose(class_weight)) ) #shape [1, batch_size]
-    crossentropy = tf.mul(weight_per_label , tf.nn.softmax_cross_entropy_with_logits(logits, labels, name="xent_raw"))
-
-    return tf.reduce_mean(crossentropy, name = 'class_cross_entropy')
-
-def softmax_cross3(y_true,y_pred):
-    class_weight = tf.constant(np.array([1, 1000], dtype='f'))
+def weighted_softmax(y_true,y_pred):
+    class_weight = tf.constant(np.array([1.0, 100], dtype='f'))
 
 
     labels = K.flatten(y_true)
@@ -138,7 +198,7 @@ def get_unet():
     #inputs = Input(( 1, img_rows, img_cols))
     inputs = Input(( img_rows, img_cols, 1))
     print('inputs:', inputs)
-    conv1 = Convolution2D(64, 3, 3, activation='relu', border_mode='same',dim_ordering="tf")(inputs)
+    conv1 = Convolution2D(64, 3, 3, activation='relu',border_mode='same',dim_ordering="tf")(inputs)
     print('conv1_1:', conv1)
     conv1 = Convolution2D(64, 3, 3, activation='relu', border_mode='same',dim_ordering="tf")(conv1)
     print('conv1_2:', conv1)
@@ -192,18 +252,18 @@ def get_unet():
     print('up7:', up7)
     conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same',dim_ordering="tf")(up7)
     print('conv7_1:', conv7)
-    conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same',dim_ordering="tf")(conv7)
+    conv7 = Convolution2D(128, 3, 3, activation='relu',border_mode='same',dim_ordering="tf")(conv7)
     print('conv7_2:', conv7)
 
     up8 = merge([Convolution2D(64, 2, 2,activation='relu', border_mode='same',dim_ordering="tf")(UpSampling2D(size=(2, 2),dim_ordering="tf")(conv7)), conv1], mode='concat', concat_axis=3)
     print('up8:', up8)
-    conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same',dim_ordering="tf")(up8)
+    conv8 = Convolution2D(64, 3, 3, activation='relu',border_mode='same',dim_ordering="tf")(up8)
     print('conv8_1', conv8)
     conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same',dim_ordering="tf")(conv8)
     print('conv8_2', conv8)
 
 
-    conv9 = Convolution2D(2, 1, 1, activation='relu',dim_ordering="tf")(conv8)
+    conv9 = Convolution2D(2, 1, 1, dim_ordering="tf")(conv8)
 
 
     print('conv9:', conv9)
@@ -213,7 +273,7 @@ def get_unet():
 
     #model.load_weights(os.getcwd()+'/u2.hdf5')
 
-    model.compile(optimizer=Adam(lr=10e-5,decay=0.001), loss = softmax_cross3, metrics=["accuracy","precision", "recall", "fmeasure",])
+    model.compile(optimizer=Adam(lr=10e-5,decay=0.001), loss = weighted_softmax, metrics=[acc1,prec1,rec1,fm1])
 
     #model.compile(optimizer=Adam(lr=10e-5), loss="binary_crossentropy", metrics=["accuracy","precision", "recall", "fscore"])
 
@@ -274,18 +334,14 @@ class Histories(Callback):
         self.val_losses.append(logs.get('val_loss'))
         self.val_accs.append(logs.get('val_acc'))
         self.losses.append(logs.get('loss'))
+
         self.accs.append(logs.get('acc'))
-        self.prec.append(logs.get('precision'))
-        self.recall.append(logs.get('recall'))
-        self.recall.append(logs.get('fscore'))
+        self.prec.append(logs.get('prec'))
+        self.recall.append(logs.get('rec'))
+        #self.recall.append(logs.get('fm'))
 
         return
 
-    def on_batch_end(self, batch, logs={}):
-        print ('\n')
-
-
-        return
  
 
         
@@ -304,14 +360,14 @@ def train(imgs_train,gt_train,model):
     print('Fitting model...')
     print('-'*30)
 
-    pandas.DataFrame( model.fit(imgs_train, gt_train, batch_size=4, nb_epoch=70, verbose=1, shuffle=True,callbacks=[model_checkpoint, history], validation_split=0.2).history).to_csv("history.csv")
+    pandas.DataFrame( model.fit(imgs_train, gt_train, batch_size=2, nb_epoch=100, verbose=1, shuffle=True,callbacks=[model_checkpoint, history], validation_split=0.2).history).to_csv("history.csv")
 
     print('-'*30)
     print('saving weights and model as .h5')
-    model.save('backend_model_70epochs.h5')
-    model.save_weights('backend_model_weights_70epochs.h5')
+    model.save('backend_model.h5')
+    model.save_weights('backend_model_weights.h5')
     print( history)
-    plt_history(history)
+    #plt_history(history)
     np.savetxt('model_history.txt', np.array(history).reshape(1,),  delimiter=" ", fmt="%s")
 
     print('-'*30)
@@ -331,6 +387,26 @@ def test(imgs_test,model):
     np.save('imgs_mask_predicted.npy', gt_test)
     return gt_test
 
+def visualize(img,label,logit):
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(131)
+    ax2 = fig.add_subplot(132)
+    ax3 = fig.add_subplot(133)
+
+    ax1.axis('off')
+    ax2.axis('off')
+    ax3.axis('off')
+    ax1.title.set_text('Image')
+    ax1.imshow(img, cmap='gray')
+    ax2.title.set_text('Ground Truth')
+    ax2.imshow(label, cmap='gray')
+    ax3.title.set_text('Predictions')
+    ax3.imshow(logit, cmap='gray')
+
+    plt.show()
+
+
 
 def main():
     print('-'*30)
@@ -345,12 +421,41 @@ def main():
     train(x_train,y_train,model)
 
 
+def maintest():
+    print('-'*30)
+    print('Loading and preprocessing test data...')
+    print('-'*30)
+    x,y = load_testdata()
+
+    slice = 0
+
+    ns= slice+1
+    img = x[slice:ns,:,:,:]
+    label = y[slice,:,:,0]
+    print("img shape: ", img.shape)
+
+    model = get_unet()
+    model.load_weights(os.getcwd()+"/Models/Model1/backend_model_weights_70epochs.h5")
+    model.compile(optimizer=Adam(lr=10e-5,decay=0.001), loss = weighted_softmax, metrics=["accuracy"])
+    logits = model.predict(img)
+    logits = logits[0,:,:,:]
+
+    print ("Logits:", logits)
+    logits = np.argmax(logits, axis=2)
+    print (logits.shape)
+    visualize(img[0,:,:,0],label,logits)
+
+
+
+
+
+
 
 
 
 
 if __name__ == '__main__':
     main()
-    ##for debuging the data
+    ##for testing
     #maintest()
 
